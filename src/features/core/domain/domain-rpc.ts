03f36131217dc75161e166da9502869073c2e6f4
Cody Wright <acodywright@gmail.com>
2025-12-23 07:13:12 +0000

    refactor: todo api and rpc into their own files

--- src/features/core/domain/domain-rpc.ts
+++ src/features/core/domain/domain-rpc.ts
@@ -1,41 +1,4 @@
-import * as Rpc from "@effect/rpc/Rpc";
 import * as RpcGroup from "@effect/rpc/RpcGroup";
-import * as Schema from "effect/Schema";
-import {
-  CreateTodoInput,
-  Todo,
-  TodoId,
-  TodoNotFound,
-  UpdateTodoInput,
-} from "@/features/todo/domain/todo-schema";
-
-export class TodosRpc extends RpcGroup.make(
-  Rpc.make("list", {
-    success: Schema.Array(Todo),
-  }),
-
-  Rpc.make("getById", {
-    success: Todo,
-    error: TodoNotFound,
-    payload: { id: TodoId },
-  }),
-
-  Rpc.make("create", {
-    success: Todo,
-    payload: { input: CreateTodoInput },
-  }),
-
-  Rpc.make("update", {
-    success: Todo,
-    error: TodoNotFound,
-    payload: { id: TodoId, input: UpdateTodoInput },
-  }),
-
-  Rpc.make("remove", {
-    success: Schema.Void,
-    error: TodoNotFound,
-    payload: { id: TodoId },
-  }),
-).prefix("todos_") {}
+import { TodosRpc } from "@/features/todo/domain";
 
 export class DomainRpc extends RpcGroup.make().merge(TodosRpc) {}
