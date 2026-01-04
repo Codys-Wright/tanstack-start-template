b8d375f13e0e8418b859794afdbca0171d7c94f9
Cody Wright <acodywright@gmail.com>
2026-01-04 03:07:13 +0000

    chore(artist-types): update package config and exports
    - Add new package exports for /server and /database
    - Update tsconfig for new source structure
    - Add main index.ts entry point

---
+++ packages/artist-types/src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @artist-types - Client and Domain exports
+ *
+ * This is the main entry point for the artist-types package.
+ * Import from '@artist-types' for client-side code.
+ *
+ * For server-side code, import from '@artist-types/server'
+ * For database operations, import from '@artist-types/database'
+ */
+
+// Domain exports
+export * from './domain/index.js';
+
+// Client exports
+export * from './client/index.js';
